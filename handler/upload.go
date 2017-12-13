496e3518f6c4c40a772dc75ce7c1d81f1b56d2e2
Sam Manzer <github@invincibleinfra.com>
2017-12-13 14:26:15 -0600

    db setup and insertion of records works

--- handler/upload.go
+++ handler/upload.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -13,8 +14,34 @@ func (h *handler) uploadHandler(c *gin.Context) {
 		c.AbortWithError(http.StatusBadRequest, err)
 		return
 	}
+	albumName := c.PostForm("albumname")
+	if albumName == "" {
+		c.AbortWithError(http.StatusBadRequest, fmt.Errorf("Album name is required"))
+		return
+	}
+
+	stmt, err := h.db.Prepare(`
+INSERT INTO photos.albums (
+	album_name,
+	photo_name
+) VALUES (?, ?);`)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
 	fileName := uploadedFile.Filename
+	_, err = stmt.Exec(
+		albumName,
+		fileName,
+	)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
 	log.Printf("File named %v was successfully uploaded.", fileName)
+
 	c.HTML(http.StatusOK, "upload.tmpl", gin.H{
 		"fileName": fileName,
 	})
---
+++ setup.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS photos;
+USE photos;
+
+CREATE TABLE IF NOT EXISTS albums (
+  album_name text,
+  photo_name text
+);
