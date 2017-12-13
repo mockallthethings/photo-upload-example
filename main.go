a55b2a35d1bde280296f2351754eb994888c0275
Sam Manzer <github@invincibleinfra.com>
2017-12-13 10:18:47 -0600

    index template basics

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
+func indexHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		"host": c.Request.Host,
+	})
+}
+
 func main() {
 	r := gin.Default()
+	r.LoadHTMLFiles("public/index.tmpl")
+	r.GET("/", indexHandler)
 	r.Run(":80")
 }
